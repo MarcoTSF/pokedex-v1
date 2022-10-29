77caf1e92c9e52c5d8fd53c30dd046fe560ca404
MarcoTSF <mtsf.salvador@gmail.com>
2022-10-29 20:06:10 -0300

    Pokédex - 0.1

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+}
+
+fetchPokemon('25')
