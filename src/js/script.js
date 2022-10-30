3b70c2be53fb891cd9012c5e36609f51aeda3812
MarcoTSF <mtsf.salvador@gmail.com>
2022-10-30 00:47:52 -0300

    Pokédex - 0.5

--- src/js/script.js
+++ src/js/script.js
@@ -33,6 +33,7 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     if (data) {
+        pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
@@ -40,7 +41,7 @@ const renderPokemon = async (pokemon) => {
         input.value = '';
         searchPokemon = data.id
     } else {
-        pokemonImage.style.display = 'block';
+        pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Pokémon inválido.';
         pokemonNumber.innerHTML = 'Erro';
     }
