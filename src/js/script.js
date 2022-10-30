12eb0099b6c76ea417c4d4f74e136186d1d73a4a
MarcoTSF <mtsf.salvador@gmail.com>
2022-10-29 21:53:16 -0300

    Pokédex - 0.3

--- src/js/script.js
+++ src/js/script.js
@@ -6,34 +6,69 @@ const pokemonImage = document.querySelector('.pokemon-image');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
 
 
 
 const fetchPokemon = async (pokemon) => {
 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
-    const data = await APIResponse.json();
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
-    return data;
+    if (APIResponse.status === 200) {
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
 
 
 const renderPokemon = async (pokemon) => {
 
+    pokemonName.innerHTML = 'Carregando...';
+    pokemonNumber.innerHTML = '';
+
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
-    ['animated']['front_default'];
-
-    input.value = '';
+    if (data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
+        ['animated']['front_default'];
+        input.value = '';
+        searchPokemon = data.id
+    } else {
+        pokemonImage.style.display = 'block';
+        pokemonName.innerHTML = 'Pokémon inválido.';
+        pokemonNumber.innerHTML = 'Erro';
+    }
 }
 
+
+
 form.addEventListener('submit', (event) => {
-
     event.preventDefault();
-
-    renderPokemon(input.value);
+    renderPokemon(input.value.toLowerCase());
 });
+
+
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1;
+        renderPokemon(searchPokemon);
+    };
+});
+
+
+
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+});
+
+
+
+renderPokemon(searchPokemon);
