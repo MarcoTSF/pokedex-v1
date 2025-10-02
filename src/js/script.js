89d026228636e5f4594b2eb0cb502d14b755dffa
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2025-10-02 11:21:35 -0300

    feat: adição das últimas gerações com imagens estáticas e remoção da limitação até o PKM 649

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,6 @@ const pokemonName = document.querySelector('.pokemon-name');
 const pokemonNumber = document.querySelector('.pokemon-number');
 const pokemonImage = document.querySelector('.pokemon-image');
 
-
-
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-search');
 const buttonPrev = document.querySelector('.btn-prev');
@@ -11,10 +9,7 @@ const buttonNext = document.querySelector('.btn-next');
 
 let searchPokemon = 1;
 
-
-
 const fetchPokemon = async (pokemon) => {
-
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
     if (APIResponse.status === 200) {
@@ -23,10 +18,7 @@ const fetchPokemon = async (pokemon) => {
     }
 }
 
-
-
 const renderPokemon = async (pokemon) => {
-
     pokemonName.innerHTML = 'Carregando...';
     pokemonNumber.innerHTML = '';
 
@@ -36,10 +28,29 @@ const renderPokemon = async (pokemon) => {
         pokemonImage.style.display = 'block';
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
-        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']
-        ['animated']['front_default'];
+
+        let sprite = data?.sprites?.versions?.['generation-v']?.['black-white']?.animated?.front_default;
+
+        if (!sprite) {
+            sprite = data.sprites.front_default;
+        }
+
+        if (!sprite) {
+            sprite = data.sprites.other['dream_world'].front_default;
+        }
+
+        if (!sprite) {
+            sprite = data.sprites.other['home'].front_default;
+        }
+
+        if (!sprite) {
+            pokemonImage.style.display = 'none';
+        } else {
+            pokemonImage.src = sprite;
+        }
+
         input.value = '';
-        searchPokemon = data.id
+        searchPokemon = data.id;
     } else {
         pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Pokémon inválido.';
@@ -47,31 +58,23 @@ const renderPokemon = async (pokemon) => {
     }
 }
 
-
-
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     renderPokemon(input.value.toLowerCase());
 });
 
-
-
 buttonPrev.addEventListener('click', () => {
     if (searchPokemon > 1) {
         searchPokemon -= 1;
         renderPokemon(searchPokemon);
-    };
+    }
 });
 
-
-
 buttonNext.addEventListener('click', () => {
-    if (searchPokemon < 649) {
+    if (searchPokemon < 1025) {
         searchPokemon += 1;
         renderPokemon(searchPokemon);
     }
 });
-
-
 
 renderPokemon(searchPokemon);
